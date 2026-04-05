7caf5eca40457205f15d0d8ffeb13d8a228caec6
Advaith-Hello <advaith.anoopr@gmail.com>
2026-04-05 12:53:18 -0500

    First

--- FirstOpenGL/main.cpp
+++ FirstOpenGL/main.cpp
@@ -2,5 +2,14 @@
 #include <GLFW/glfw3.h>
 
 
+int main() {
+	// Init command
+	glfwInit();
+	// Sets to OpenGL v3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Sets profile to core
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
+	return 0;
+}
